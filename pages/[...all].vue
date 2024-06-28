4d878e54942a24c5c5b40f311ca5049d1c21eeee
Seedorf Rai <dorf123see@gmail.com>
2024-06-28 15:02:06 +0545

    Error Page added

---
+++ pages/[...all].vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex py-5 justify-center items-center w-full h-[270px] object-contain">
+        <img src="https://www.codeit.com.np/asset/img/pana.png" class="h-[250px] object-cover" alt="">
+    </div>
+</template>
